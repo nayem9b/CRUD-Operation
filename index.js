0f09f48ae915e1d571e3f4fda8fa816787abc33b
nayem9b <razwanniam1@gmail.com>
2022-11-04 04:28:47 +0600

    crud delete method added

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ const app = express();
 const cors = require("cors");
 require("colors");
 require("dotenv").config();
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // Middlewere
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER, process.env.DB_PASSWORD);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dafmrk2.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
 const client = new MongoClient(uri);
 async function run() {
   try {
@@ -41,6 +39,12 @@ app.post("/product", async (req, res) => {
       });
     }
   } catch {}
+});
+
+app.delete("/product/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findOne({ _id: ObjectId(id) });
+  const result = await Product.deleteOne({ _id: ObjectId(id) });
 });
 
 app.get("/product", async (req, res) => {
